51dbd7955f6341275d0edef910674a021341214b
Arusey <kevinkibetw7@gmail.com>
2018-11-01 10:27:41 +0300

    [Feature #161580727]Add failing tests and login endpoint

--- app/api/v2/views.py
+++ app/api/v2/views.py
@@ -198,3 +198,23 @@ class Product(Resource):
         return make_response(jsonify({
             "Message": "Please login first"
         }), 401)
+class SingleProduct(Resource):
+    @token_required
+    def get(current_user, self, id):
+        '''gets single product'''
+        product = ModelProduct()
+        products = product.get()
+        if len(products) == 0:
+            return make_response(jsonify({
+                "Message": "No products have been posted yet"
+            }), 404)
+        if current_user:
+            for product in products:
+                if int(id) == product["id"]:
+                    return make_response(jsonify({
+                    "Message": "Product retrieval successful",
+                    "product": product
+                    }), 200)
+        return make_response(jsonify({
+            "Message": "This product does not exist"
+        }), 404)
