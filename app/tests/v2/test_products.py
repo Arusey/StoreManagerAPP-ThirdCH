ef54516723c2a73fb70b1ba099653a5f3cb2c68d
Arusey <kevinkibetw7@gmail.com>
2018-11-01 10:11:32 +0300

    [Feature #161580647]Add failing tests for get all sales

--- app/tests/v2/test_products.py
+++ app/tests/v2/test_products.py
@@ -18,3 +18,14 @@ class TestProducts(TestAllEndpoints):
         message = json.loads(response.data)
         self.assertEqual(message["Message"], "Product posted successfully")
         self.assertEqual(response.status_code, 201)
+    def test_get_all_products(self):
+        response = self.test_client.get("/api/v2/products",
+                                         data=self.product,
+                                         headers={
+                                            'content-type': 'application/json',
+                                            'x-access-token': self.token_for_admin
+                                         })
+        message = json.loads(response.data)
+
+        self.assertEqual(message["Message"], "All products fetched successfully")
+        self.assertEqual(response.status_code, 200)
