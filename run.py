50af2163021e2d3e89b6afa719834456c200066a
Arusey <kevinkibetw7@gmail.com>
2018-11-11 13:28:53 +0300

    Add few changes

--- run.py
+++ run.py
@@ -3,5 +3,8 @@ from app.api.v2.models.databaseModel import Db
 app = create_app("development")
 # db_obj = Db()
 # db_obj.create_tables()
+@app.route("/")
+def index():
+    return "<h1>find the app documentation</h1><a href="https://documenter.getpostman.com/view/4790487/RzZ4q21V">here</a></p>"
 if __name__ ==  '__main__':
     app.run()
